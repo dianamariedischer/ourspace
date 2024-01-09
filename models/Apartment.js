391bf0eedc2912ab88b34100ced148be7c11420f
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-08 20:11:49 -0500

    updated apartment model

---
+++ models/Apartment.js
@@ -0,0 +1,84 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Apartment extends Model {}
+
+Apartment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    filename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address2: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.INT,
+      allowNull: false,
+    },
+    date_added: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    rent: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    beds: {
+      type: DataTypes.INT,
+      allowNull: false,
+    },
+    baths: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INT,
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Apartment;
