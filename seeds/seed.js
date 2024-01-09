660c58a23182c9e82448d9c3ce169f8609c31661
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-08 21:24:11 -0500

    Completed models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, ApartmentCollection, Apartment } = require('../models');
 
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
