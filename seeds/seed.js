7fcd713da244bf019f671e74728becb676ce4abd
nsetty <nsett7388@gmail.com>
2024-01-08 20:14:00 -0500

    created package.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Project, Apartment } = require('../models');
 
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
+const apartmentData = require('./ApartmentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
