b5c38dda84dae811b9c8a7eec4b4fda2669085fb
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-09 21:57:44 -0500

    added css and linked to main, and added public folder to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const path = require('path');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -29,6 +30,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
