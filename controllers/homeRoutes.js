3f36f1f56d87f55aaa2e456ed201b48ca6e85d72
L.lar <l.lar1109@gmail.com>
2024-01-11 19:43:08 -0500

    created function for signup handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -23,4 +23,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+
+// Signup route
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
 module.exports = router;
