026a23d62c910a246d9abe41e34a2d71c42b01a1
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-17 06:31:43 -0500

    comment display added

--- seeds/CommentData.js
+++ seeds/CommentData.js
@@ -3,9 +3,29 @@ const { Comment } = require('../models');
 const commentData = [
   {
     text: 'I would enjoy living here!',
+    user_id: 2,
+    apartment_id: 1,
+  },
+  {
+    text: 'I think so too',
     user_id: 1,
     apartment_id: 1,
   },
+  {
+    text: ':)',
+    user_id: 2,
+    apartment_id: 1,
+  },
+  {
+    text: 'I would enjoy living here!',
+    user_id: 2,
+    apartment_id: 3,
+  },
+  {
+    text: 'I would enjoy living here!',
+    user_id: 2,
+    apartment_id: 4,
+  }
 ];
 
 const seedComment = () => Comment.bulkCreate(commentData);
