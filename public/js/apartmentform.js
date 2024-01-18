11fdcc30dd630cfc1a25ef8d052b179ca226d488
Diana Discher <50310682+dianamariedischer@users.noreply.github.com>
2024-01-18 08:35:47 -0500

    apartment form accessing stars for ratings in form

--- public/js/apartmentform.js
+++ public/js/apartmentform.js
@@ -1,3 +1,5 @@
+let rating = 0;
+
 const apartmentFormHandler = async (event) => {
     event.preventDefault();
   
@@ -10,7 +12,6 @@ const apartmentFormHandler = async (event) => {
     const rent = document.querySelector('#rent').value.trim();
     const beds = document.querySelector('#bed').value.trim();
     const baths = document.querySelector('#bath').value.trim();
-    const rating = document.querySelector('#rating').value.trim();
     const link = document.querySelector('#link').value.trim();
     const notes = document.querySelector('#notes').value.trim();
     const imageLink = document.querySelector('#image-link').value.trim();
@@ -40,13 +41,29 @@ const apartmentFormHandler = async (event) => {
         });
   
         if (response.ok) {
-            document.location.replace('/landingpage');
+            document.location.replace('/apartmentcollection/' + apartment_collection_id);
         } else {
             alert('Failed to add apartment.');
         }
     }
 };
   
+
+function log (event) {
+    rating = event.target.value;
+    console.log(rating);
+}
+
+function addEventListenerList(list, event, fn) {
+    for (i = 0; i < list.length; i++) {
+        list[i].addEventListener(event, fn);
+    }
+}
+
+ratingNodeList = document.querySelectorAll('.rate');
+console.log(ratingNodeList)
+addEventListenerList(ratingNodeList, 'click', log);
+
 document
     .querySelector('.apartment-form')
     .addEventListener('submit', apartmentFormHandler);
